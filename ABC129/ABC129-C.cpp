88aa124d04a76045e63743039fa220414ee02083
Prai18 <daichi.sino@gmail.com>
2019-06-11 11:28:03 +0900

    ABC129

--- ABC129/ABC129-C.cpp
+++ ABC129/ABC129-C.cpp
@@ -1,41 +1,46 @@
 /*
+問題文
+N段の階段があります。高橋君は現在、上り口(0段目)にいます。
+高橋君は一歩で 1段か 2段上ることができます。
+ただし、a1,a2,a3,....aM段目の床は壊れており、その段に足を踏み入れることは危険です。
+壊れている床を踏まないようにしながら、最上段(N段目)にたどりつくまでの移動方法は何通りあるでしょうか？
+総数を 1,000,000,007で割った余りを求めてください。
+
+制約
+1≦N≦10^5
+0≦M≦N−1
+1≦a1<a2< ... <a_M≦N−1
 */
 
-#include<iostream>
-#include<stdio.h>
-#include <algorithm>
+#include<bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;i++)		//repマクロ
 
 using namespace std;
 
 int main(){
-
 	int n,m;
 	cin>>n>>m;
 
-	int a[m];	//壊れた床
-	int pattern;
-	// for (int i = 0; i < m; ++i)
-	// {
-	// 	cin>>a[i];		
-	// }
-	
-	int b[n];
+	vector<bool> broken(n+1);
 
-	b[0]=0;
-	b[1]=1;
-	b[2]=2;
-	b[3]=3;
-
-	for (int i = 3; i < n; ++i)
-	{
-		pattern+=b[i-1];
-
+	rep(i,m){
+		int a;
+		cin>>a;
+		broken[a]=true;
 	}
 
-
-	printf("%d\n",pattern);
-	
+	vector<int> dp(n+2);
+	const int mod=1000000007;
+	dp[n]=1;
+	for (int i = n-1; i >= 0; --i)
+	{
+		if(broken[i]){
+			dp[i]=0;
+			continue;
+		}
+		dp[i]=(dp[i+1]+dp[i+2])%mod;
+		printf("%d\n",dp[i]);
+	}
+	cout<<dp[0]<<endl;
 	return 0;
 }
-
-
